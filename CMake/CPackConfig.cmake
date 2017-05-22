bb88876884db95b53d7a0732f045245ff7656db9
Raphael Dumusc <raphael.dumusc@epfl.ch>
2017-05-22 16:59:10 +0200

    Integrate CMake/common as a git submodule

--- CMake/CPackConfig.cmake
+++
@@ -1,9 +0,0 @@
-# Copyright (c) HBP 2015 Stefan Eilemann <eile@eyescale.ch>
-# All rights reserved. Do not distribute without further notice.
-
-# General CPack configuration
-# Info: http://www.itk.org/Wiki/CMake:Component_Install_With_CPack
-
-set(CPACK_PACKAGE_DESCRIPTION_FILE "${PROJECT_SOURCE_DIR}/README.md")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libavahi-client3, avahi-daemon")
-include(CommonCPack)
